68c670bc28685d24a123d177edc3cbfd527ec426
nastya <nastya_babenko2005@mail.ru>
2021-11-23 14:47:13 +1000

    generate circle prog

---
+++ main.py
@@ -0,0 +1,41 @@
+import sys
+from random import randint
+
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+
+        self.flag = False
+
+        self.pushButton.clicked.connect(self.draw)
+
+    def draw(self):
+        self.flag = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.flag:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_circle(qp)
+            qp.end()
+            self.flag = False
+
+    def draw_circle(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        rad = randint(1, 250)
+        qp.drawEllipse(100, 10, rad, rad)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    wnd = MyWindow()
+    wnd.show()
+    sys.exit(app.exec())
+
